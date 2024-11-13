740675a561b5082c78340dbeaf831bf8adb56907
Pippo-commits <amministrazione@newyoungfactory.com>
2024-11-13 14:35:14 +0100

    preRelease Push

---
+++ app/page.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>
+        New Young Factory | Costruiamo il futuro digitale insieme a te | Web e
+        Video production
+      </title>
+
+      <div id="preReleaseBody">
+        <Image
+          id="preReleaseImage"
+          src={"/SVG/Risorsa1.svg"}
+          alt="suca"
+          width={500}
+          height={500}
+          className="scale-in-center"
+        ></Image>
+        <h1 id="preReleaseH1" className="tracking-in-contract">
+          Coming Soon
+        </h1>
+      </div>
+    </>
+  );
+}
